21b639d0a475475ee284746d36a9a2227d32f6b9
Ula Kocur <ula@Mac.lan>
2025-01-09 03:54:38 +0100

    Added comments

--- project3/functions.c
+++ project3/functions.c
@@ -1,5 +1,7 @@
 #include "functions.h"
 
+//ALLOCATE 2D ARRAYS
+// Function to allocate memory for a 2D array, where m = number of rows and n = numbers of columns
 double** malloc_2d(size_t m, size_t n) {
 	double** a = malloc(m*sizeof(double*));
 	if (a == NULL) {
@@ -16,19 +18,23 @@ double** malloc_2d(size_t m, size_t n) {
 	return a;
 }
 
+//FREE 2D ARRAYS
+//Free previously allocated memeory for a 2D array
 void free_2d(double** a) {
 	free(a[0]);
 	a[0] = NULL;
 	free(a);
 }
 
-//NUMBER OF ATOMS
+//READING NUMBER OF ATOMS
+//Check if the input file was opened correctly, exit the program with an error code otherwise
 size_t read_Natoms(FILE* input_file) {
     if (input_file == NULL) {
         printf("Error opening input file\n");
         exit(-1);
     }
 
+//Read the number of atoms from the first line
     size_t number_of_atoms;
     int read_number = fscanf(input_file, "%zu", &number_of_atoms);
     if (read_number != 1) {
@@ -40,10 +46,12 @@ size_t read_Natoms(FILE* input_file) {
 }
 
 //READING COORDINATES AND MASS
+//Read coordinates and mass from the input file
 void read_molecule(FILE* input_file, size_t Natoms, double** coord, double* mass) {
     for (size_t i = 0; i < Natoms; i++) {
         int read_number = fscanf(input_file, "%lf %lf %lf %lf", &coord[i][0], &coord[i][1], &coord[i][2], &mass[i]);
-        if (read_number != 4) {
+//Exit the program if the number of columns in the input file is different than 4
+	if (read_number != 4) {
             printf("Error: Couldn't read mass and coordinates\n");
             exit(-1);
         }
@@ -51,6 +59,7 @@ void read_molecule(FILE* input_file, size_t Natoms, double** coord, double* mass
 }
 
 //COMPUTE DISTANCES BETWEEN ATOMS
+//Calculates the distances between all pairs of atoms
 void compute_distances(size_t Natoms, double** coord, double** distances) {
 	for (size_t i = 0; i < Natoms; i++) {
 		for (size_t j = 0; j < Natoms; j++) {
@@ -68,6 +77,7 @@ void compute_distances(size_t Natoms, double** coord, double** distances) {
 }
 
 //COMPUTE POTENTIAL ENERGY
+//Computes the Lennard-Jones potential energy for all pairs of atoms, checks if all atoms have distinct positions
 double V(size_t Natoms, double** distance) {
 	double pot_E = 0.0;
 	
@@ -91,6 +101,7 @@ double V(size_t Natoms, double** distance) {
 }
 
 //COMPUTE KINETIC ENERGY
+//Calculates the kinetic energy of each atom
 double T(size_t Natoms, double** velocity, double* mass) {
 	double kin_E = 0.0;
 
@@ -102,7 +113,8 @@ double T(size_t Natoms, double** velocity, double* mass) {
 	return kin_E;
 }
 
-//TOTAL ENERGY
+//COMPUTE TOTAL ENERGY
+//Adds kinetic and potential energy
 double E(size_t Natoms, double** distance, double** velocity, double* mass) {
 	double pot_E = V(Natoms, distance);
 	double kin_E = T(Natoms, velocity, mass);
@@ -110,7 +122,8 @@ double E(size_t Natoms, double** distance, double** velocity, double* mass) {
 	return tot_E;
 }
 
-//ACCELERATION
+//COMPUTE ACCELERATION
+//Calculates acceleration vectors for each atom
 void compute_acc(size_t Natoms, double** coord, double* mass, double** distance, double** acceleration) {
         for (size_t i = 0; i < Natoms; i++) {
 		for (size_t d =0; d < 3; d++) {
@@ -135,6 +148,7 @@ void compute_acc(size_t Natoms, double** coord, double* mass, double** distance,
         }
 }
 
+//UPDATING THE POSITIONS FOR THE VERLET ALGORITHM
 void update_position(size_t Natoms, double** coord, double** velocity, double** acceleration, double** distances, double dt) {
 	for (size_t i = 0; i < Natoms; i++) {
 		for (size_t d = 0; d < 3; d++) {
@@ -143,6 +157,7 @@ void update_position(size_t Natoms, double** coord, double** velocity, double** 
 	}
 }
 
+//UPDATING THE VELOCITIES FOR THE VERLET ALGORITHM
 void update_velocity(size_t Natoms, double** coord, double** velocity, double** acceleration, double* mass, double** distances, double dt) {
 	for (size_t i = 0; i < Natoms; i++) {
 		for (size_t d = 0; d < 3; d++) {
